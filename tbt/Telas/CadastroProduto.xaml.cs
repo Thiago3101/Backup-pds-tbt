5f565f8596bc1ab5894300495b49bfe00e1fc2b4
Thiago Samasquini <116303614+Thiago3101@users.noreply.github.com>
2024-11-17 19:47:44 -0400

    Conexão

--- tbt/Telas/CadastroProduto.xaml.cs
+++ tbt/Telas/CadastroProduto.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using tbt.Models;
 
 namespace tbt.Telas
 {
@@ -22,6 +23,27 @@ namespace tbt.Telas
         public CadastroProduto()
         {
             InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string tipo_produto = tipo_prod.Text;
+            string marca = marca_prod.Text;
+            string modelo = modelo_prod.Text;
+            string referencia = refencia_prod.Text;
+            string descricao = descricao_prod.Text;
+            double preco_custo = Convert.ToDouble(preco_custo_prod.Text);
+            double valor_aluguel = Convert.ToDouble(valor_aluguel_prod.Text);
+            string cor = cor_prod.Text;
+
+            Produto produto = new Produto(marca, tipo_produto, modelo, cor, preco_custo, referencia, descricao, valor_aluguel);
+            ProdutoDAO produtoDAO = new ProdutoDAO();
+            produtoDAO.Insert(produto);
         }
     }
 }
